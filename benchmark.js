9b3bc53f62f8e742f82d14a85e2d8b2ecb65d2d7
Nate Fischer <ntfschr@gmail.com>
2016-02-28 20:24:27 -0800

    Adjusting README and system info output.

--- benchmark.js
+++ benchmark.js
@@ -7,6 +7,8 @@ var shouldLog = (process.argv[2] === 'log');
 var log = [];
 
 function writeLog(msg, link) {
+  if (!msg)
+    msg = '';
   console.log(msg);
   if (link) {
     msg = msg.replace(']', '](' + link + ')');
@@ -16,21 +18,39 @@ function writeLog(msg, link) {
 
 function printSystemInfo() {
   var os = require('os');
-  var cpus = os.cpus();
-  var spawn = require('child_process').spawn;
-  console.log('System Information:');
-  console.log(os.type(), os.release(), os.arch());
-  console.log('Node.js ' + process.versions.node);
-  console.log('V8 ' + process.versions.v8);
-  console.log(cpus[0].model + ' × ' + cpus.length);
-  console.log();
-  console.log('Shell Information:');
-  spawn('sh', ['-c', '${SHELL} --version'], { stdio: 'inherit' });
+  var cpuInfo = os.cpus().reduce(function (cpus, cur) {
+    var idx = 0;
+    cpus.forEach(function (thisCpu) {
+      if (thisCpu.model === cur.name) return;
+      idx++;
+    });
+    if (idx !== cpus.length)
+      cpus[idx].count++;
+    else
+      cpus.push({name: cur.model, count: 1});
+    return cpus;
+  }, []);
+  writeLog('## System Information:');
+  writeLog(' - ' + os.type());
+  writeLog(' - ' + os.release());
+  writeLog(' - ' + os.arch());
+  cpuInfo.forEach(function (cpu) {
+    writeLog(' - ' + cpu.name + ' × ' + cpu.count);
+  });
+  writeLog('## Node information');
+  writeLog(' - Node.js: ' + exec('node -v').stdout.trim());
+  writeLog(' - V8: ' + process.versions.v8);
+  writeLog();
+  writeLog('## Shell Information:');
+  writeLog(' - name: `' + exec('bash -c \'echo ${SHELL}\'', {silent: true}).stdout + '`');
+  writeLog(' - version: ' + exec('bash --version', {silent: true}).stdout.replace(/\n+/g, '\n'));
 }
 
 cd(__dirname + '/' + TEST_DIR);
 var prefix;
 var shellJSWins = [];
+printSystemInfo();
+
 ls().forEach(function (dir) {
   prefix = TEST_DIR + '/' + dir;
   writeLog('### [' + dir + ']', prefix)
@@ -96,4 +116,3 @@ if (shouldLog) {
   // Append new docs to README
   sed('-i', /## Results/, '## Results\n\n' + text, 'README.md');
 }
-printSystemInfo();
