7285747c2bde2a1a4efdc56405187fd3a6035f88
Nate Fischer <ntfschr@gmail.com>
2016-03-02 20:25:59 -0800

    Install script now bootstraps, so everything works

--- scripts/install.js
+++ scripts/install.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
-// Use v0.6.0, since this has no runtime dependency
-require('../shelljs/0.6.0');
-cd('shelljs');
-ls().forEach(function (k) {
-  cd(k);
-  exec('npm install --production');
-  cd('..');
+
+// install the first version manually so we have something to use
+var curDir = process.cwd();
+process.chdir(__dirname + '/../shelljs/0.6.0');
+var c = require('child_process').exec('npm install --production', function(code, stdout) {
+  console.log(stdout);
+  // Use v0.6.0, since this has no runtime dependency
+  require('../shelljs/0.6.0');
+  cd(__dirname + '/../shelljs');
+  ls().forEach(function (k) {
+    cd(k);
+    exec('npm install --production');
+    cd('..');
+  });
 });
