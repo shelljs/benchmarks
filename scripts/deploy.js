d99e958e8208569c952b3d49e74690edb7cf9052
Travis CI <ntfschr@gmail.com>
2016-03-03 01:42:08 -0800

    Another attempt to fix gh-pages deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,7 +12,9 @@ if (env.GH_TOKEN) {
   exec('git config user.email "ntfschr@gmail.com"');
   exec('git fetch origin gh-pages:gh-pages');
   exec('git add .');
+  exec('git stash');
   exec('git checkout gh-pages');
+  exec('git stash pop');
   exec('git commit -m "Deploy to Github pages"');
   var ret = exec('git push --force --quiet "https://${GH_TOKEN}@github.com/shelljs/benchmarks.git" gh-pages').code;
   if (ret === 0) {
