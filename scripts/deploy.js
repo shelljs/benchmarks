740f990fb78c65a7ca528a44f63ea351ff7fb5ad
Travis CI <ntfschr@gmail.com>
2016-03-03 01:15:26 -0800

    Refactoring gh-pages deployment

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+require('../shelljs/0.6.0');
+
+// Deploy to Github pages
+// This script is typically run by the CI
+// GH_TOKEN must be an environmental variable
+
+set('-e');
+if (env.GH_TOKEN) {
+  console.log('Deploying to Github pages');
+  exec('git config user.name "Travis CI"');
+  exec('git config user.email "ntfschr@gmail.com"');
+  exec('git fetch origin gh-pages:gh-pages');
+  exec('git checkout gh-pages');
+  exec('git add .');
+  exec('git commit -m "Deploy to Github pages"');
+  var ret = exec('git push --force --quiet "https://${GH_TOKEN}@github.com/shelljs/benchmarks.git" gh-pages').code;
+  if (ret === 0) {
+    console.log('Successfully deployed!');
+  } else {
+    console.error('Error deploying to Github pages');
+  }
+  exec('git checkout -'); // back to previous branch
+  exit(ret);
+} else {
+  console.error('Error: GH_TOKEN must be in the current environment');
+  exit(1);
+}
--- scripts/install.js
+++ scripts/install.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
 // install the first version manually so we have something to use
-var curDir = process.cwd();
 process.chdir(__dirname + '/../shelljs/0.6.0');
 var c = require('child_process').exec('npm install --production', function(code, stdout) {
   console.log(stdout);
-  // Use v0.6.0, since this has no runtime dependency
   require('../shelljs/0.6.0');
+  set('-e');
+  cd(__dirname + '/..');
+  exec('git submodule init');
+  exec('git submodule update');
   cd(__dirname + '/../shelljs');
   ls().forEach(function (k) {
     cd(k);
